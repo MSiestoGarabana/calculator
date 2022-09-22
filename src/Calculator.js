d0dfb4da62bf1eee37fc3f537338139ebb55a3d5
MSiesto <miguelsiesto@gmail.com>
2022-09-22 15:47:05 +0200

    Css updates

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,10 +3,12 @@ import KeyBooard from "./Keyboard";
 import "./Calculator.css";
 
 function Calculator() {
+  let [input, setInput] = React.useState(0);
+  let [output, setOutput] = React.useState(9);
   return (
     <div id="Calculator">
-      <output className="Calculator__output">output</output>
-      <input className="Calculator__input"></input>
+      <output className="Calculator__output">{output}</output>
+      <input className="Calculator__input" value={0}></input>
       <KeyBooard className="Calculator__keyboard" />
     </div>
   );
