a612a1e8314fc92e89a6475899a390c37f3541a6
MSiesto <miguelsiesto@gmail.com>
2022-09-28 12:50:05 +0200

    implementing some calculator logic

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,14 +3,20 @@ import CalculatorKeyPad from "./CalculatorKeyPad";
 import "./Calculator.css";
 
 function Calculator() {
-  let [output, setOutput] = React.useState("");
+  let [output, setOutput] = React.useState("0");
   let [input, setInput] = React.useState("0");
-  console.log(input);
 
   const handleClick = (symbol) => {
-    if (input === "0") {
+    if (symbol === "=") {
+      console.log("this will be the result");
+      setOutput(input);
+    } else if (symbol === "AC") {
+      setInput("0");
+    } else if (input === "0" && symbol !== ".") {
       setInput(symbol);
-    } else setInput(input + symbol);
+    } else {
+      setInput(input + symbol);
+    }
   };
 
   return (
