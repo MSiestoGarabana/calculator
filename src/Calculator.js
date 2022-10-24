f4ba514c896f9c5f73bc38b8280a83d9da53de94
MSiesto <miguelsiesto@gmail.com>
2022-10-24 19:29:52 +0200

    debugging logic + eval method

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,66 +1,20 @@
 import React from "react";
 import CalculatorKeyPad from "./CalculatorKeyPad";
 import "./Calculator.css";
-import CalculatorInputLogic from "./CalculatorInputLogic";
+import { getCalculatornputLogic } from "./CalculatorInputLogic";
 
 function Calculator() {
   let [output, setOutput] = React.useState("0");
   let [input, setInput] = React.useState("0");
 
   const handleClick = (symbol) => {
-    if (symbol === "=") {
-      console.log("this will be the result");
-    } else if (symbol === "AC") {
-      setInput("0");
-      setOutput("0");
-    } else if (
-      // no "+ X /" if input is 0
-      input === "0" &&
-      /[X/+-]/.test(symbol) === true
-    ) {
-      setInput(input + symbol);
-      console.log("u cant start with this kind of operator");
-    } else if (
-      /[+X/]-/.test(input) === true &&
-      /[+\-X/]/.test(symbol) === true
-    ) {
-      setInput(input.slice(0, -2) + symbol);
-      console.log("You found me");
-    } else if (input.at(-1) === "-" && /[+\-X/]/.test(symbol) === true) {
-      setInput(input.slice(0, -1) + symbol);
-      console.log("hey");
-    } else if (
-      //End of a number ? replace "." with symbol if symbol === operator
-      /[+\-X/]/.test(symbol) === true &&
-      splittedInput.at(-1).at(-1) === "."
-    ) {
-      setInput(input.slice(0, -1) + symbol);
-    } else if (
-      //no "." after an operator
-      symbol === "." &&
-      splittedInput.at(-1) === ""
-    ) {
-      console.log("U cant place a dot after an operator character");
-    } else if (
-      // u cant place two operators in a row (unless it´s a minus after another operator)
-      /[+X/]/.test(symbol) === true &&
-      /[+\-X/]/.test(input.at(-1)) === true
-    ) {
-      setInput(input.slice(0, -1) + symbol);
-    } else if (
-      //no double "." in a number
-      symbol === "." &&
-      /\./.test(splittedInput.at(-1)) === true
-    ) {
-      console.log("U cant place 2 dots in the same number");
-    } else if (input === "0" && symbol !== ".") {
-      setInput(symbol);
-    } else {
-      setInput(input + symbol);
+    const { newInput, newOutput } = getCalculatornputLogic(input, symbol);
+    setInput(newInput);
+    if (newOutput) {
+      setOutput(newOutput);
     }
   };
-  let splittedInput = input.split(/[+\-X/]/);
-  console.log(input.at(-1));
+
   return (
     <div id="Calculator">
       <div className="Calculator__output">{output}</div>
---
+++ src/CalculatorInputLogic.js
@@ -0,0 +1,82 @@
+export function getCalculatornputLogic(input, symbol) {
+  const splittedInput = input.split(/[+\-X/]/);
+  if (symbol === "=") {
+    if (doesInputContainX(input) === true) {
+      let newInput = input.replace(/X/g, "*");
+
+      let integerResult = eval(newInput);
+      let stringResult = integerResult.toString();
+      return { newInput: stringResult, newOutput: input };
+    }
+    let integerResult = eval(input);
+    let stringResult = integerResult.toString();
+    return { newInput: stringResult, newOutput: input };
+  } else if (isSymbolAC(symbol) === true) {
+    return { newInput: "0", newOutput: "0" };
+  } else if (isInput0(input) === true && isSymbolAnOperator(symbol) === true) {
+    return { newInput: input + symbol };
+  } else if (
+    doesInputFinishWithpPlusMultiplicationOrDivisionBeforeMinus(input) ===
+      true &&
+    isSymbolAnOperator(symbol) === true
+  ) {
+    return { newInput: input.slice(0, -2) + symbol };
+  } else if (
+    isPreviousInputMinus(input) === true &&
+    isSymbolAnOperator(symbol) === true
+  ) {
+    return { newInput: input.slice(0, -1) + symbol };
+  } else if (isSymbolAnOperator(symbol) === true && input.at(-1) === ".") {
+    return { newInput: input.slice(0, -1) + symbol };
+  } else if (isSymbolDot === true && splittedInput.at(-1) === "") {
+    console.log("U cant place a dot after an operator character");
+    return { newInput: input };
+  } else if (
+    isSymbolMultiplicationDivisionOrPlus(symbol) === true &&
+    doesInputFinishWithOperator(input) === true
+  ) {
+    return { newInput: input.slice(0, -1) + symbol };
+  } else if (
+    isSymbolDot(symbol) === true &&
+    isThereDotInCurrentNumber(splittedInput) === true
+  ) {
+    console.log("U cant place 2 dots in the same number");
+    return { newInput: input };
+  } else if (isInput0(input) === true && isSymbolDot(symbol) === false) {
+    return { newInput: symbol };
+  } else {
+    return { newInput: input + symbol };
+  }
+}
+
+function isSymbolDot(symbol) {
+  return symbol === "." ? true : false;
+}
+
+function isSymbolAC(symbol) {
+  return symbol === "AC" ? true : false;
+}
+function isInput0(input) {
+  return input === "0" ? true : false;
+}
+function isSymbolAnOperator(symbol) {
+  return /[+\-X/]/.test(symbol) ? true : false;
+}
+function isPreviousInputMinus(input) {
+  return input.at(-1) === "-" ? true : false;
+}
+function isSymbolMultiplicationDivisionOrPlus(symbol) {
+  return /[X/+]/.test(symbol) ? true : false;
+}
+function doesInputContainX(input) {
+  return /X/.test(input) ? true : false;
+}
+function doesInputFinishWithpPlusMultiplicationOrDivisionBeforeMinus(input) {
+  return /[+X/]-/.test(input) ? true : false;
+}
+function doesInputFinishWithOperator(input) {
+  return /[+\-X/]/.test(input.at(-1)) ? true : false;
+}
+function isThereDotInCurrentNumber(splittedInput) {
+  return /\./.test(splittedInput.at(-1)) ? true : false;
+}
